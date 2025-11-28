0d154cfa918c36dda138a9f335c83c059f99905f
Ryan <writdev@gmail.com>
2025-11-29 05:29:43 +0700

    add log viewer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,11 +17,11 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: null,
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        apiPrefix: 'ipn',
+        apiPrefix: env('API_PREFIX', 'api'),
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(replace: [
